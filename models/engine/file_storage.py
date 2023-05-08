edd1f6e588ddfb22f2ff48939b35737e7859d149
Cyril-777 <72940048+Cyril-777@users.noreply.github.com>
2023-05-09 02:16:55 +0300

    Update file_storage.py
    reload public instance is still missing.

---
+++ models/engine/file_storage.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+
+import json
+import models
+
+
+class FileStorage:
+    """
+    most simple implementation of storage,
+    which stores data in a flat file
+    serializes instances to a JSON file
+    and deserializes JSON file to instances
+    """
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Returns the dictionary __objects"""
+        return self.__objects
+
+    def new(self, obj):
+        """Sets in __objects the obj with key <obj class name>.id"""
+        key = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key] = obj
+
+    def save(self):
+        """Serializes __objects to the JSON file"""
+        serialized_objects = {}
+        for key, obj in self.__objects.items():
+            serialized_objects[key] = obj.to_dict()
+        with open(self.__file_path, mode='w', encoding="UTF8") as json_file:
+            json.dump(serialized_objects, json_file)
+
+    def reload(self):
+        """Deserializes the JSON file to __objects"""
